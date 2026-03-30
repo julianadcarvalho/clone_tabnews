68613f1c746f1883ef690234841341723e0ff318
julianadcarvalho <julianafsilvaju@gmail.com>
2026-03-30 10:10:30 -0300

    feat: add migration

--- infra/database.js
+++ infra/database.js
@@ -1,30 +1,36 @@
 import { Client } from 'pg'
 
 async function query(querObject) {
+  const isProduction = process.env.NODE_ENV === 'production';
+
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DATABASE,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: true
+    ...(isProduction && {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }),
   });
-  await client.connect()
+
+  await client.connect();
 
   try {
-    const result = await client.query(querObject)
+    const result = await client.query(querObject);
     return result;
 
   } catch (error) {
-    console.error(error)
-    throw error
+    console.error(error);
+    throw error;
 
   } finally {
-
-    await client.end()
+    await client.end();
   }
 }
 
 export default {
-  query: query,
+  query,
 };
