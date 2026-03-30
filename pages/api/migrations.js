a39cbf69b485f642b9cd212be6e8d015c463224d
julianadcarvalho <julianafsilvaju@gmail.com>
2026-03-30 12:01:14 -0300

    migrations

--- pages/api/migrations.js
+++ pages/api/migrations.js
@@ -1,33 +1,33 @@
 import migrationRunner from "node-pg-migrate";
-import { join } from "path";
 
 export default async function migrations(request, response) {
-
-  const databaseUrl = `postgresql://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DATABASE}`
+  const databaseUrl = process.env.DATABASE_URL;
 
   if (request.method === "GET") {
     const migrations = await migrationRunner({
-      databaseUrl: databaseUrl,
+      databaseUrl,
       dryRun: true,
-      dir: join("infra", "migrations"),
+      dir: "infra/migrations",
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
       noLock: true,
     });
+
     return response.status(200).json(migrations);
   }
 
   if (request.method === "POST") {
     const migrations = await migrationRunner({
-      databaseUrl: databaseUrl,
+      databaseUrl,
       dryRun: false,
-      dir: join("infra", "migrations"),
+      dir: "infra/migrations",
       direction: "up",
       verbose: true,
       migrationsTable: "pgmigrations",
       noLock: true,
     });
+
     return response.status(200).json(migrations);
   }
 }
