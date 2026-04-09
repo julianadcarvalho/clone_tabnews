403ad2e6af5abbaedf0ffebef07701132bb1bb7e
julianadcarvalho <julianafsilvaju@gmail.com>
2026-04-09 15:01:49 -0300

    feat: status

--- pages/api/status.js
+++ pages/api/status.js
@@ -1,33 +1,41 @@
-import { version } from "react"
-import database from "../../infra/database.js"
+import { createRouter } from "next-connect";
+import database from "infra/database";
+import controller from "infra/controller.js";
 
-async function status(request, response) {
-  const updatedAt = new Date().toISOString()
+const router = createRouter();
 
-  const databaseVersionResult = await database.query("SHOW server_version;")
-  const databaseVersionValue = (databaseVersionResult.rows[0].server_version)
+router.get(getHandler);
 
-  const databaseMaxConnectionsResult = await database.query("SHOW max_connections")
-  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections
+export default router.handler(controller.errorHandlers);
 
+async function getHandler(request, response) {
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionsResult = await database.query({
-    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1",
-    values: [process.env.POSTGRES_DATABASE]
-  })
-
-  const databaseOpenedConnectionsValues = databaseOpenedConnectionsResult.rows[0].count
-
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
-        max_connections: databaseMaxConnectionsValue,
-        opened_connections: databaseOpenedConnectionsValues
-      }
-    }
-  })
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
-
-export default status;
