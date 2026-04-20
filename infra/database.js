457ac4bda8b8b192f189909288d997cd64c33a85
julianadcarvalho <julianafsilvaju@gmail.com>
2026-04-20 16:13:02 -0300

    feat: add controller and refactor database

--- infra/database.js
+++ infra/database.js
@@ -1,40 +1,48 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
+
+async function query(queryObject) {
+  let client;
+  const result = await client.query(queryObject);
+  return result;
+} catch (error) {
+  const serviceErrorObject = new ServiceError({
+    message: "Erro na conexão com Banco ou na Query.",
+    cause: error,
+  });
+  throw serviceErrorObject;
+} finally {
+  await client?.end();
+}
+}
 
 async function getNewClient() {
-  const isProduction = process.env.NODE_ENV === "production";
-
   const client = new Client({
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DATABASE,
+    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ...(isProduction && {
-      ssl: {
-        rejectUnauthorized: false,
-      },
-    }),
+    ssl: getSSLValues(),
   });
 
   await client.connect();
   return client;
 }
 
-async function query(queryObject) {
-  const client = await getNewClient();
-
-  try {
-    const result = await client.query(queryObject);
-    return result;
-  } catch (error) {
-    console.error(error);
-    throw error;
-  } finally {
-    await client.end();
-  }
-}
-
-export default {
+const database = {
   query,
-  getNewClient
+  getNewClient,
 };
+
+export default database;
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+
+  return process.env.NODE_ENV === "production" ? true : false;
+}
